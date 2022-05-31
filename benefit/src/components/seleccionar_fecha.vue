6c495fe832efe48274b5fe1407f59b7c211e5408
SadelIT <sadel.fortunato@atdev.do>
2022-05-31 16:07:00 -0400

    update

--- benefit/src/components/seleccionar_fecha.vue
+++ benefit/src/components/seleccionar_fecha.vue
@@ -1,5 +1,14 @@
 <template>
-<div></div>
+    <div id='container' style="margin:50px auto 0; width:250px;">
+        <br><br>
+        <label class="custom-input-label">Mask support with default format</label>
+        <br><br>
+        <input id='mask' />
+        <br><br><br>
+        <label class="custom-input-label">Mask support with custom format</label>
+        <br><br>
+        <input id='format' />
+    </div>
 
   
   
@@ -10,6 +19,7 @@ export default {
   name: 'SeleccionarFecha',
   data() {
     return {
+        
 
 
     }
