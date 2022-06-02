c67a7c53e72a02915e0b5791b758588db14cd0ab
SadelIT <102696518+SadelIT@users.noreply.github.com>
2022-06-02 17:07:23 -0400

    5:07

--- benefit/src/components/ingresar_datos.vue
+++ benefit/src/components/ingresar_datos.vue
@@ -1,103 +1,75 @@
 <template>
-<div>
-<input type="number" :value="value0" @input="numberone = $event.target.value" >
-<input type="number" :value="value1" @input="numbertwo = $event.target.value" >
-<button v-on:click="operacion()">Resolver</button>
-<p>Health Discounts: {{mensaje}}</p>
-<p>Social Security Discounts: {{mensaje2}}</p>
-<p>Taxes Discounts: {{mensaje3}}</p>
-<p>Taxes Discounts: {{mensaje4}}</p>
-<p>Discounts: {{mensaje5}}</p>
-<p>Net Salary: {{mensaje5}}</p>
-</div>
-
-<<<<<<< HEAD
-  
-
-=======
-  <p>ID: {{ ID }}</p>
-  <input type="number" v-model="ID" placeholder="Your ID" />
-
-  <p>NAME: {{ Name }}</p>
-  <input type="text" v-model="Name" placeholder="Your Name" />
-
-  <p>COMPANY: {{ Company }}</p>
-  <input type="text" v-model="Company" placeholder="Your Company" />
-
-  <h1>Seleccione La fecha de inicio y finalizacion laboral </h1>
-
-<label for="start">Fecha de Inicio:</label>
-
-<input type="date" id="inicio" name="trip-start"
-       value=""
-       min="" max="">
+  <div>
+  <input type="number" :value="value0" @input="salary = $event.target.value" >
 
 
-       <label for="start">Fecha de Salida:</label>
+  <button v-on:click="operacion()">Resolver</button>
 
-<input type="date" id="salida" name="trip-start"
-       value=""
-       min="" max="">
-
-
-       <h1>Seleccione el período</h1>
-
-<input type="radio" name="test" value="mensual"> Mnesual
-<input type="radio" name="test" value="Quincenal"> Quincenal
-<input type="radio" name="test" value="Semanal"> Semanal
-<input type="radio" name="test" value="Diario"> Diario
-
-
-<h1>Seleccione el tipo de calculo </h1>
-
-<input type="radio" name="test" value="Ordinario"> Ordinario
-<input type="radio" name="test" value="Intermitente"> Intermitente
-
-<p>Jornada ordinaria es la ejecutada por trabajadores dentro de un período que no exceda de ocho (8) horas al día ni de cuarenta y cuatro (44) a la semana.</p>
->>>>>>> 29a6e1132f7f49e858ee90ea03bedca91f7f9f5b
+  <p>Salary: {{Salary1}}</p>
+  <p>Health Discounts: {{Health}}</p>
+  <p>Social Security Discounts: {{Social}}</p>
+  <p>Taxes Discounts: {{Taxes}}</p>
+  <p>Discounts: {{Discounts}}</p>
+  <p>Net Salary: {{NetSalary}}</p>
+  </div>
 
   
   
 </template>
 
 <script>
-  
-  export default {
-  
-  
+export default {
   name: 'IngresarDatos',
-<<<<<<< HEAD
   data: function () {
         return {
-            numberone: null,
-            numbertwo: null,
-            mensaje: null,
-            mensaje2: null,
-            mensaje3: null
+            salary: null,
+            Health: null,
+            Social: null,
+            Taxes: null,
+            Discounts: null,
+            NetSalary: null
         }
     },
     methods: {
         operacion() {
-            this.mensaje = parseInt(this.numberone) +
-                parseInt(this.numbertwo);
-            this.mensaje2 = parseInt(this.numberone) -
-                parseInt(this.numbertwo);
-            this.mensaje3 = parseInt(this.numberone) *
-                parseInt(this.numbertwo);
-        }
-    },
-=======
-  data() {
-    return {
-      ID: '',
-      Name: '',
-      Company: ''
+            
 
+
+
+            this.Salary1 = (this.salary)
+            
+            this.Health = parseInt(this.salary) *
+                (3.04 / 100);
+                
+            this.Social = parseInt(this.salary) *
+                (2.87 / 100)
+
+
+            this.Discounts = parseInt(this.Health) +
+                parseInt(this.Social);
+
+            this.NetSalary = parseInt(this.salary) -
+                (this.Discounts);
+
+            this.Taxes = parseInt(this.salary) -
+             parseInt(this.Discounts)
+
+            
+
+            //if (this.salary >= 416220.01 / 12  || this.salary == 624329.00){
+            //this.Taxes = (this.NetSalary) * (0.15 / 100)
+
+          //} 
+
+          //this.Discounts = parseInt(this.Discounts) +
+            //Math.round(this.Taxes)
+
+           //this.NetSalary = parseInt(this.salary) -
+                //Math.round(this.Discounts)
+          
+
+          
+        }
     }
-  },
-  props: {
-    msg: String
-  }
->>>>>>> 29a6e1132f7f49e858ee90ea03bedca91f7f9f5b
 }
 </script>
