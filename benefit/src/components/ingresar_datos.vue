36498d68e8a2ffd8dac0f05653df456b4e55e90d
SadelIT <sadel.fortunato@atdev.do>
2022-06-06 17:30:10 -0400

    5:30

--- benefit/src/components/ingresar_datos.vue
+++ benefit/src/components/ingresar_datos.vue
@@ -1,209 +1,95 @@
- <template>
+<template>
 
-    <div class="card"  id="Calculo">
-    <div class="container">
 
-    <p>Enter your salary </p>
 
-    <p> 
-    <input class="inpu" type="number" :value="vlaue0"  @input="salary = $event.target.value" >
-    </p>
+
+
+<div class="info">
+
+<p>ID: {{ id }}</p>
+<input type="number" v-model="id" placeholder="ID" />  
+
+<p>Name: {{ name }}</p>
+<input v-model="name" placeholder="Name" />  
+
+<p>Company: {{ company }}</p>
+<input v-model="company" placeholder="Company" />  
+
+
+</div>
+<br>
+
+
+<div class="datelaboral">
+  <p> Start Date:</p>
+ <input class="startdate" type="date" v-model="startdate">
+ <p> {{startdate}} </p>
+
+  <p> End Date:</p>
+  <input class="enddate" type="date" v-model="enddate">
+ <p> {{enddate}} </p>
 
   
+ </div>
 
-        <p>Salary: {{Salary1}}</p>
-        <p>Health Discounts: {{Health}}</p>
-        <p>Social Security Discounts: {{Social}}</p>
-    
-        <p>Taxes Discounts monthly: {{Taxes}}</p>
+
   
-  
-        <p>Discounts: {{Discounts}}</p>
-        <p>Net Salary: {{NetSalary}}</p>
-
-        <div class="control">
-        <p>
-        <button v-on:click="operacion()" class="btnC">Calculate</button>
-        
-        <button @click="reset" class="btnR">Reset</button> 
-        </p>
-        </div>
-
-        
-    </div>
-    </div>
-
 </template>
 
-<script>
-export default {
+<script>  
+
+  //START JAVASCRIPT
+  export default {
   name: 'IngresarDatos',
   data: function () {
         return {
-            salary: null,
-            Health: null,
-            Social: null,
-            Taxes: null,
-            Discounts: null,
-            NetSalary: null,
-            AnnualSalary: null
+            id: "",
+            name: "",
+            company: "",
+            date: '',
+            startdate: null,
+            enddate: null,
+            mydate: '',
+  
+            starts: '',
+            moment: '',
+
+
         }
     },
-    methods: {
-        reset() {
-       this.Salary1 = '',
-       this.Health = '',
-       this.Social = '',
-       this.Taxes = '',
-       this.Discounts = '',
-       this.NetSalary = '',
-       this.AnnualSalary = ''
-    },
-        
-        operacion: function() {
-            
+    methods: function() {
 
 
-
-            this.Salary1 = (this.salary)
-            
-            this.Health = parseInt(this.salary) *
-                (3.04 / 100);
-                
-            this.Social = parseInt(this.salary) *
-                (2.87 / 100)
-
-
-
-            this.Discounts = parseInt(this.Health) +
-                parseInt(this.Social);
-
-            this.NetSalary = parseInt(this.salary) -
-                (this.Discounts);
-             
-            this.AnnualSalary = parseInt(this.NetSalary) * (12).toFixed(1)
-
-
-
-            if ( this.AnnualSalary >= 416220.01){
-            this.Taxes = parseInt(this.AnnualSalary) -  (416220)
-
-            
-              this.Taxes = (this.Taxes) *  (0.15)
-              this.Taxes = ((this.Taxes) /  (12)).toFixed(2)
-
-                 console.log(this.Taxes)
-                if( this.AnnualSalary >= 624329.01){
-                this.Taxes = (this.AnnualSalary) - (624329)
-
-            
-                this.Taxes = (this.Taxes) *  (0.20) + (2601.33)
-                this.Taxes = ((this.Taxes) /  (12)).toFixed(2)
-
-                console.log(this.Taxes)
-                }
-
-                if( this.AnnualSalary >= 867123.01){
-                this.Taxes = (this.AnnualSalary) -  (867123)
-
-            
-                this.Taxes = (this.Taxes) *  (0.25) + (6648)
-                this.Taxes = ((this.Taxes) /  (12)).toFixed(2)
-
-                console.log(this.Taxes)
-                }
-
-
-
-             this.Discounts = parseInt(this.Discounts) + parseInt(this.Taxes)  
-
-             this.NetSalary = parseInt(this.salary) - parseInt(this.Discounts)
-
-
-            } 
-
-            
-            ///else if( this.AnnualSalary >= 624329.01){
-            //this.Taxes = (this.AnnualSalary) - (416220)
-
-            
-            //this.Taxes = (this.Taxes) *  (0.20)
-            //this.Taxes = (this.Taxes) /  (12)
-
-            //console.log(this.Taxes + 'im')
-            //}
-            
-            //else if (this.AnnualSalary > 624329.01){
-              //  this.Taxes = 'hello'
-
-             // }
-
-
-
-
-            
-
-
-
-          //this.Discounts = parseInt(this.Discounts) +
-            //(this.Taxes)
-
-           //this.NetSalary = parseInt(this.salary) -
-                //(this.Discounts)
-          
-        
-          
-        }
-        
+      
     }
-}
+    
+    
+  }
+
+  //END JAVASCRIPT
+
 </script>
 
 
 
 <style scoped>
 
-.card {
- 
-  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-  transition: 0.3s;
-  margin: 300px;
-  margin-top: 0;
-  align: center
+
+
+
+
+
+
+
+p {
+  font-weight: bold;
+
 }
 
-
-.card:hover {
-  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-}
-
-
-.container {
-    
+.date{
   
-  
-
-}
-
-p {
-  text-align: center;
 }
 
 
-
-.btnC {
-    padding: 0.5em 1em;
-  border: none;
-  background: rgb(100, 200, 255);
-  cursor: pointer;
-}
-
-.btnR {
-    margin: 5px;
-    padding: 0.5em 1em;
-  border: none;
-  background: rgb(255,0,0);
-  cursor: pointer;
-}
 
 </style>
--- benefit/src/components/ingresar_salario.vue
+++ benefit/src/components/ingresar_salario.vue
@@ -9,22 +9,15 @@ export default {
   name: 'IngresarSalario',
   data() {
       return{
-          n1: 0,
-          n2: 0
+         
       }
 
   },
   computed: {
-   sum2: function () {
-     console.log('prop-> ' + this.n1 + '+' + this.n2);
-     return this.n1 + this.n2;
-   }
+
  },
  methods: {
-   sum: function () {
-     console.log('method-> ' + this.n1 + '+' + this.n2);
-     return this.n1 + this.n2;
-   }
+   
  }
 
 
