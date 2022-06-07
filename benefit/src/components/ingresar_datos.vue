649bc2002c6f3fd7f39f71c4284902f8e2b33424
SadelIT <sadel.fortunato@atdev.do>
2022-06-07 17:57:15 -0400

    5:57

--- benefit/src/components/ingresar_datos.vue
+++ benefit/src/components/ingresar_datos.vue
@@ -1,10 +1,13 @@
 <template>
+  <p>Salary: {{Salary1}}</p>
 
-
-
-
+  <p> 
+    <input class="inpu" type="number"   @input="salary = $event.target.value" >
+    </p>
 
 <div class="info">
+
+  <p> soy result: {{result}}</p>
 
 <p>ID: {{ id }}</p>
 <input type="number" v-model="id" placeholder="ID" />  
@@ -29,8 +32,44 @@
   <input class="enddate" type="date" v-model="enddate">
  <p> {{enddate}} </p>
 
-  
+
+ <p>mostrar prueba: {{preAvisado}}</p>
+
+  <div class="calculate info">
+
+ <!-- <input type="checkbox"  v-model="Ordinary" /> Ordinary
+	<label for="checkbox">{{  }}</label>
+
+  <input type="checkbox"  v-model="intermittent" />Intermittent
+	<label for="checkbox">{{  }}</label> -->
+
+  <input type="checkbox"  v-model="monthly" /> Monthly
+	<label for="checkbox">{{  }}</label>
+
+  <input type="checkbox"  v-model="fortnightly" /> Fortnightly
+	<label for="checkbox">{{  }}</label>
+
+  <input type="checkbox"  v-model="weekly" /> Weekly
+	<label for="checkbox">{{  }}</label>
+
+  <input type="checkbox"  v-model="diary" /> Diary
+	<label for="checkbox">{{  }}</label>
+
+  </div>
+
+ <button v-on:click="operacion()" class="btn btn-primary">Calculate</button>
+
+
  </div>
+
+
+
+
+
+
+  
+
+ 
 
 
   
@@ -43,25 +82,84 @@
   name: 'IngresarDatos',
   data: function () {
         return {
+
+          //START VAR OF CALCULATE
+            weekly: null,
+            fortnightly: null,
+            diary: null,
+            monthly: null,
+          //END VAR CALCULATE
+            Total: "",  
+            salary: null,
+            Salary1: null,
+            salary01: null,
+            comission01: null,
+
+          //START VAR INFO
             id: "",
             name: "",
             company: "",
+          //END VAR CALCULATE
+
+
+          //START VAR DATE  
             date: '',
             startdate: null,
             enddate: null,
             mydate: '',
-  
+          //END VAR DATE
+
+            preAvisado: '',
             starts: '',
             moment: '',
 
 
+            picked: '',
+            result: null
+
+
         }
     },
-    methods: function() {
+    methods:  {
+
+
+    
+
+      operacion: function(){
+
+        
+
+        
+      
+
+
+        if(this.monthly == true){
+          this.result = parseInt(this.salary) + (10)
+        }
+        else{
+           this.result = 0
+        }
+
+
+        
+
+
+  
+
+
+
+
+
+      }
+
+     
+
+
 
 
       
     }
+
     
     
   }
@@ -76,10 +174,30 @@
 
 
 
+.container{
+  background-color: transparent;
+    border: none;
+    border-bottom: 1px solid #9e9e9e;
+    border-radius: 0;
+    outline: none;
+    height: 3rem;
+    width: 100%;
+    font-size: 1rem;
+    margin: 0 0 20px 0;
+    padding: 0;
+    box-shadow: none;
+    box-sizing: content-box;
+    transition: all .3s;
 
 
 
+}
 
+
+
+td{
+  background-color: ;
+}
 
 p {
   font-weight: bold;
--- benefit/src/components/ingresar_salario.vue
+++ benefit/src/components/ingresar_salario.vue
@@ -1,7 +1,104 @@
 <template>
-
-<div></div>
   
+ <div class="card" style="width: 18rem;">
+
+  <div class="container">
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Salary</th>
+      <th scope="col">Comissions</th>
+      <th scope="col">Total</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td> <input type="text" placeholder="0.00"  @input="salary01 = $event.target.value" /></td>
+      <td><input type="text" placeholder="0.00"  @input="comission01 = $event.target.value" /></td>
+      <td>{{Total1}}</td>
+      
+      
+
+      
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td><input type="text" placeholder="0.00" /></td>
+      <td><input type="text" placeholder="0.00" /></td>
+      <td>{{Total}}</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+        <td><input type="text" placeholder="0.00" /></td>
+        <td><input type="text" placeholder="0.00" /></td>
+      <td>{{Total}}</td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+        <td><input type="text" placeholder="0.00" /></td>
+        <td><input type="text" placeholder="0.00" /></td>
+      <td>{{Total}}</td>
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+        <td><input type="text" placeholder="0.00" /></td>
+        <td><input type="text" placeholder="0.00" /></td>
+      <td>{{Total}}</td>
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+        <td><input type="text" placeholder="0.00" /></td>
+        <td><input type="text" placeholder="0.00" /></td>
+      <td>{{Total}}</td>
+    </tr>
+    <tr>
+      <th scope="row">7</th>
+        <td><input type="text" placeholder="0.00" /></td>
+        <td><input type="text" placeholder="0.00" /></td>
+      <td>{{Total}}</td>
+    </tr>
+        <tr>
+      <th scope="row">8</th>
+        <td><input type="text" placeholder="0.00" /></td>
+        <td><input type="text" placeholder="0.00" /></td>
+      <td>{{Total}}</td>
+    </tr>
+        <tr>
+      <th scope="row">9</th>
+        <td><input type="text" placeholder="0.00" /></td>
+        <td><input type="text" placeholder="0.00" /></td>
+      <td>{{Total}}</td>
+    </tr>
+        <tr>
+      <th scope="row">10</th>
+        <td><input type="text" placeholder="0.00" /></td>
+        <td><input type="text" placeholder="0.00" /></td>
+      <td>{{Total}}</td>
+    </tr>
+        <tr>
+      <th scope="row">11</th>
+        <td><input type="text" placeholder="0.00" /></td>
+        <td><input type="text" placeholder="0.00" /></td>
+      <td>{{Total}}</td>
+    </tr>
+        <tr>
+      <th scope="row">12</th>
+        <td><input type="text" placeholder="0.00" /></td>
+        <td><input type="text" placeholder="0.00" /></td>
+      <td>{{Total}}</td>
+    </tr>
+  </tbody>  
+  </table>
+ </div>
+
+  
+</div> 
+
+       <button v-on:click="operacion()" class="btn btn-primary">Calculate</button>
+
+
 </template>
 
 <script>
@@ -9,14 +106,36 @@ export default {
   name: 'IngresarSalario',
   data() {
       return{
+         Total1: null,
+        Total: null,
+        salary01: '',
+        comission01: '',
          
       }
 
   },
-  computed: {
-
- },
  methods: {
+
+   operacion: function(){
+
+     this.Total1 = (this.salary01) + (this.comission01)
+
+        
+
+        
+      
+
+
+        
+
+
+  
+
+
+
+
+
+      }
    
  }
 
